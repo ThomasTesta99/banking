8531e80ca5f97b525b1406b18276ec4ccb3f6c97
Thomas Testa <ttesta99@yahoo.com>
2024-10-21 15:02:51 -0400

    implement transaction history and my banks

--- components/ui/PlaidLink.tsx
+++ components/ui/PlaidLink.tsx
@@ -3,6 +3,7 @@ import { Button } from './button'
 import { PlaidLinkOptions, PlaidLinkOnSuccess, usePlaidLink } from 'react-plaid-link'
 import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const PlaidLink = ({user, variant}: PlaidLinkProps) => {
     const router = useRouter();
@@ -44,12 +45,26 @@ const PlaidLink = ({user, variant}: PlaidLinkProps) => {
                 Connect Bank
             </Button>
         ): variant === 'ghost' ? (
-            <Button>
-                Connect bank
+            <Button onClick = {() => open()} variant = "ghost"
+                className = "plaidlink-ghost">
+                <Image 
+                    src = "/icons/connect-bank.svg"
+                    alt = "connect bank"
+                    width = {24}
+                    height = {24}
+                />
+                <p className = "text-[16px] font-semibold text-black-2 xl:block">Connect bank</p>
             </Button>
          ): (
-            <Button>
-                Connect bank
+            <Button onClick = {() => open()}
+                className = "plaidlink-default">
+                <Image 
+                    src = "/icons/connect-bank.svg"
+                    alt = "connect bank"
+                    width = {24}
+                    height = {24}
+                />
+                <p className = "text-[16px] font-semibold text-black-2">Connect bank</p>
             </Button>
          )}
     </>
--- components/ui/RecentTransactions.tsx
+++ components/ui/RecentTransactions.tsx
@@ -10,7 +10,7 @@ const RecentTransactions = ({
     accounts, 
     transactions = [],
     appwriteItemId,
-    page = 1,
+    //page = 1,
 }: RecentTransactionsProps) => {
   return (
     <section className="recent-transactions">
@@ -18,7 +18,7 @@ const RecentTransactions = ({
             <h2 className = "recent-transactions-label">
                 Recent Transactions
             </h2>
-            <Link href = {`/transactions-history/?id=${appwriteItemId}`} className = "view-all-btn">
+            <Link href = {`/transaction-history/?id=${appwriteItemId}`} className = "view-all-btn">
                 View all
             </Link>
         </header>
